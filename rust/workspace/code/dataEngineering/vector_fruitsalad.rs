1868bdf72df5ae6d2f653b0da0b566fa906e304b
ST4RGUARD <thema11ock@gmail.com>
2025-07-18 16:06:51 -0500

    hashMap and vectors

---
+++ rust/workspace/code/dataEngineering/vector_fruitsalad.rs
@@ -0,0 +1,37 @@
+/*
+This program creates a fruit salad by scrambling (shuffling) a list of fruit.
+A vector is a growable array. It can grow or shrink in size and is one of the most
+useful data structures in Rust. A vector is represented using the Vec<T> type.
+*/
+
+use rand::seq::SliceRandom; // rand is a random number generation library in Rust
+use rand::rng;
+
+fn main() {
+    let mut fruit = vec![
+        "Orange",
+        "Fig",
+        "Pomegranate",
+        "Cherry",
+        "Apple",
+        "Pear",
+        "Peach",
+    ];
+
+    // Scramble (shuffle) the fruit
+    let mut rng = rng();
+    fruit.shuffle(&mut rng);
+
+    // Print out the fruit salad
+    println!("Fruit Salad:");
+    for (i, item) in fruit.iter().enumerate() {
+        if i != fruit.len() - 1 {
+            match *item {
+                "Cherry" => println!("{}, ", item),
+                _ => println!("not a cherry"),
+            }
+        } else {
+            println!("{}", item);
+        }
+    }
+}

817de84c9fa02ea82e12ab61e35afd19b3f60fb2
ST4RGUARD <thema11ock@gmail.com>
2025-07-21 08:00:57 -0500

    Delete rust/workspace/dataEngineering/cli-salad/target directory

--- rust/workspace/dataEngineering/cli-salad/target/debug/deps/anstyle_parse-5631284ba74a3127.d
+++
@@ -1,9 +0,0 @@
-/home/nil/git/learn2code/rust/workspace/dataEngineering/cli-salad/target/debug/deps/anstyle_parse-5631284ba74a3127.d: /home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/lib.rs /home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/params.rs /home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/mod.rs /home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/definitions.rs /home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/table.rs
-
-/home/nil/git/learn2code/rust/workspace/dataEngineering/cli-salad/target/debug/deps/libanstyle_parse-5631284ba74a3127.rmeta: /home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/lib.rs /home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/params.rs /home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/mod.rs /home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/definitions.rs /home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/table.rs
-
-/home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/lib.rs:
-/home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/params.rs:
-/home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/mod.rs:
-/home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/definitions.rs:
-/home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/anstyle-parse-0.2.7/src/state/table.rs:
--- rust/workspace/dataEngineering/cli-salad/target/debug/deps/clap_lex-714aa34d06c8323d.d
+++
@@ -1,6 +0,0 @@
-/home/nil/git/learn2code/rust/workspace/dataEngineering/cli-salad/target/debug/deps/clap_lex-714aa34d06c8323d.d: /home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-0.7.5/src/lib.rs /home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-0.7.5/src/ext.rs
-
-/home/nil/git/learn2code/rust/workspace/dataEngineering/cli-salad/target/debug/deps/libclap_lex-714aa34d06c8323d.rmeta: /home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-0.7.5/src/lib.rs /home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-0.7.5/src/ext.rs
-
-/home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-0.7.5/src/lib.rs:
-/home/nil/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/clap_lex-0.7.5/src/ext.rs:

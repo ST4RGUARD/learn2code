d0da45a7f95f1b3f4b6e8d3626c7623114560ec0
nilclass <thema11ock@gmail.com>
2025-08-17 19:58:16 -0500

    remove react dir

--- ruby/test.rb
+++ ruby/test.rb
@@ -1,11 +1,2 @@
-# another comment replacing
-server started at 2022-07-31 14:46:56 -0500
-server started at 2022-07-31 14:46:57 -0500
-server started at 2022-07-31 14:46:58 -0500
-server started at 2022-07-31 14:46:59 -0500
-server started at 2022-07-31 14:47:00 -0500
-server started at 2022-07-31 14:47:01 -0500
-server started at 2022-07-31 14:47:02 -0500
-server started at 2022-07-31 14:47:03 -0500
-server started at 2022-07-31 14:47:04 -0500
-server started at 2022-07-31 14:47:05 -0500
+nums = gets.chomp.split.map(&:to_i)
+puts(nums[0] + nums[1])

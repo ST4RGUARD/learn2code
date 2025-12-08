53747f574c041eaa5bb3a3908a7cb87b7a332ec7
ST4RGUARD <thema11ock@gmail.com>
2025-12-08 09:47:37 -0600

    order book class

---
+++ cpp/london_oop/OrderBook.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "CSVReader.h"
+#include "OrderBookEntry.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class OrderBook {
+public:
+  /** construct reading a CSV file */
+  OrderBook(string filename);
+  /** return vector of all known products */
+  vector<string> getKnownProducts();
+  /** return vector of orders according to the sent filters */
+  vector<OrderBookEntry> getOrders(OrderBookType type, string product,
+                                   string timestamp);
+};

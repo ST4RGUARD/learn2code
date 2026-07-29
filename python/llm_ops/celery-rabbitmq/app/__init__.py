686d160b5fb17c5e51b60b3d32a417494b12d20a
ST4RGUARD <thema11ock@gmail.com>
2026-07-29 13:21:36 -0500

    rabbitmq-celery llm_ops exercise updated

---
+++ python/llm_ops/celery-rabbitmq/app/__init__.py
@@ -0,0 +1,7 @@
+"""rabbitmq.app package.
+
+This file makes the rabbitmq.app directory a proper Python package so
+imports like `rabbitmq.app.main` and relative imports inside the package work.
+"""
+
+__all__ = []
